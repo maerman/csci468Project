0d7d3a46220bd8ca3daf394d87aa8ae33233a786
Unknown <tls5200@gmail.com>
2018-04-09 15:18:51 -0400

    Fixed bug with header.

--- build/generated/src/org/apache/jsp/index_jsp.java
+++ build/generated/src/org/apache/jsp/index_jsp.java
@@ -17,10 +17,20 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     _jspx_dependants.add("/WEB-INF/jspf/footer.jspf");
   }
 
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_sql_query_var_dataSource;
+
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public java.util.List<String> getDependants() {
     return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_sql_query_var_dataSource = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_sql_query_var_dataSource.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -47,88 +57,77 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_out = out;
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
-      out.write("\n");
-      out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n");
-      out.write("    \"http://www.w3.org/TR/html4/loose.dtd\">\n");
-      out.write("\n");
-      out.write("<html>\n");
-      out.write("    <head>\n");
-      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
-      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"css/affablebean.css\">\n");
-      out.write("        <title>Project</title>\n");
-      out.write("    </head>\n");
-      out.write("    <body>\n");
-      out.write("        <div id=\"main\">\n");
-      out.write("            <div id=\"header\">\n");
-      out.write("                <div id=\"widgetBar\">\n");
-      out.write("\n");
-      out.write("                    <input id=\"searchWidget\" type=\"text\" placeholder=\"Search\">\n");
-      out.write("\n");
-      out.write("                    <div class=\"headerWidget\">\n");
-      out.write("                        [ language toggle ]\n");
-      out.write("                    </div>\n");
-      out.write("\n");
-      out.write("                    <div class=\"headerWidget\">\n");
-      out.write("                        [ checkout button ]\n");
-      out.write("                    </div>\n");
-      out.write("\n");
-      out.write("                    <div class=\"headerWidget\">\n");
-      out.write("                        [ shopping cart widget ]\n");
-      out.write("                    </div>\n");
-      out.write("\n");
-      out.write("                    <div class=\"headerWidget\">\n");
-      out.write("                        [ account widget ]\n");
-      out.write("                    </div>\n");
-      out.write("                </div>\n");
-      out.write("\n");
-      out.write("                <a href=\"#\">\n");
-      out.write("                    <img src=\"#\" id=\"logo\" alt=\"logo\">\n");
-      out.write("                </a>\n");
-      out.write("\n");
-      out.write("                <img src=\"#\" id=\"logoText\" alt=\"title\">\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\r\n");
+      out.write("    \"http://www.w3.org/TR/html4/loose.dtd\">\r\n");
+      out.write("\r\n");
+      out.write("<html>\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
+      out.write("        <link rel=\"stylesheet\" type=\"text/css\" href=\"css/affablebean.css\">\r\n");
+      out.write("        <title>Project</title>\r\n");
+      out.write("    </head>\r\n");
+      out.write("    <body>\r\n");
+      out.write("        <div id=\"main\">\r\n");
+      out.write("            <div id=\"header\">\r\n");
+      out.write("                <div id=\"widgetBar\">\r\n");
+      out.write("\r\n");
+      out.write("                    <input id=\"searchWidget\" type=\"text\" placeholder=\"Search\">\r\n");
+      out.write("\r\n");
+      out.write("                    <div class=\"headerWidget\">\r\n");
+      out.write("                        [ language toggle ]\r\n");
+      out.write("                    </div>\r\n");
+      out.write("\r\n");
+      out.write("                    <div class=\"headerWidget\">\r\n");
+      out.write("                        [ checkout button ]\r\n");
+      out.write("                    </div>\r\n");
+      out.write("\r\n");
+      out.write("                    <div class=\"headerWidget\">\r\n");
+      out.write("                        [ shopping cart widget ]\r\n");
+      out.write("                    </div>\r\n");
+      out.write("\r\n");
+      out.write("                    <div class=\"headerWidget\">\r\n");
+      out.write("                        [ account widget ]\r\n");
+      out.write("                    </div>\r\n");
+      out.write("                </div>\r\n");
+      out.write("\r\n");
+      out.write("                <a href=\"<c:url value='/'/>\">\r\n");
+      out.write("                    <img src=\"/AffableBean/img/logo.png\" id=\"logo\" alt=\"logo\">\r\n");
+      out.write("                </a>\r\n");
+      out.write("\r\n");
+      out.write("                <img src=\"/AffableBean/img/ambient.png\" id=\"logoText\" alt=\"title\">\r\n");
       out.write("            </div>");
-      out.write("<sql:query var=\"categories\" dataSource=\"jdbc/affablebean\">\n");
-      out.write("    SELECT * FROM category\n");
-      out.write("</sql:query>\n");
+      out.write('\n');
+      out.write('\n');
+      if (_jspx_meth_sql_query_0(_jspx_page_context))
+        return;
       out.write("\n");
-      out.write("            <div id=\"indexLeftColumn\">\n");
-      out.write("                <div id=\"welcomeText\">\n");
-      out.write("                    <p>[ welcome text ]</p>\n");
       out.write("\n");
-      out.write("                    <!-- test to access context parameters -->\n");
-      out.write("                    categoryImagePath: ");
-      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${initParam.categoryImagePath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
-      out.write("\n");
-      out.write("                    productImagePath: ");
-      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${initParam.productImagePath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
-      out.write("\n");
-      out.write("                </div>\n");
-      out.write("            </div>\n");
-      out.write("\n");
-      out.write("            <div id=\"indexRightColumn\">\n");
-      out.write("                <c:forEach var=\"category\" items=\"");
+      out.write("<div id=\"indexRightColumn\">\n");
+      out.write("    <c:forEach var=\"category\" items=\"");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${categories.rows}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("\">\n");
-      out.write("                    <div class=\"categoryBox\">\n");
-      out.write("                        <a href=\"category?");
+      out.write("        <div class=\"categoryBox\">\n");
+      out.write("            <a href=\"category?");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${category.id}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("\">\n");
       out.write("\n");
-      out.write("                            <span class=\"categoryLabelText\">");
+      out.write("                <span class=\"categoryLabelText\">");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${category.name}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("</span>\n");
       out.write("\n");
-      out.write("                            <img src=\"");
+      out.write("                <img src=\"");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${initParam.categoryImagePath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${category.name}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write(".jpg\"\n");
-      out.write("                                 alt=\"");
+      out.write("                     alt=\"");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${initParam.categoryImagePath}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${category.name}", java.lang.String.class, (PageContext)_jspx_page_context, null));
-      out.write("\">\n");
-      out.write("                        </a>\n");
-      out.write("                    </div>\n");
-      out.write("                </c:forEach>\n");
-      out.write("            </div>");
+      out.write(".jpg\">\n");
+      out.write("            </a>\n");
+      out.write("        </div>\n");
+      out.write("    </c:forEach>\n");
+      out.write("</div>");
       out.write("<div id=\"footer\">\n");
       out.write("                <div class=\"footerWidget\">\n");
       out.write("                        [ help widget ]\n");
@@ -155,5 +154,50 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
     }
+  }
+
+  private boolean _jspx_meth_sql_query_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  sql:query
+    org.apache.taglibs.standard.tag.rt.sql.QueryTag _jspx_th_sql_query_0 = (org.apache.taglibs.standard.tag.rt.sql.QueryTag) _jspx_tagPool_sql_query_var_dataSource.get(org.apache.taglibs.standard.tag.rt.sql.QueryTag.class);
+    _jspx_th_sql_query_0.setPageContext(_jspx_page_context);
+    _jspx_th_sql_query_0.setParent(null);
+    _jspx_th_sql_query_0.setVar("categories");
+    _jspx_th_sql_query_0.setDataSource(new String("jdbc/affablebean"));
+    int[] _jspx_push_body_count_sql_query_0 = new int[] { 0 };
+    try {
+      int _jspx_eval_sql_query_0 = _jspx_th_sql_query_0.doStartTag();
+      if (_jspx_eval_sql_query_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+        if (_jspx_eval_sql_query_0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE) {
+          out = _jspx_page_context.pushBody();
+          _jspx_push_body_count_sql_query_0[0]++;
+          _jspx_th_sql_query_0.setBodyContent((javax.servlet.jsp.tagext.BodyContent) out);
+          _jspx_th_sql_query_0.doInitBody();
+        }
+        do {
+          out.write("\n");
+          out.write("    SELECT * FROM category\n");
+          int evalDoAfterBody = _jspx_th_sql_query_0.doAfterBody();
+          if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+            break;
+        } while (true);
+        if (_jspx_eval_sql_query_0 != javax.servlet.jsp.tagext.Tag.EVAL_BODY_INCLUDE)
+          out = _jspx_page_context.popBody();
+          _jspx_push_body_count_sql_query_0[0]--;
+      }
+      if (_jspx_th_sql_query_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_sql_query_0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_sql_query_0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_sql_query_0.doFinally();
+      _jspx_tagPool_sql_query_var_dataSource.reuse(_jspx_th_sql_query_0);
+    }
+    return false;
   }
 }
